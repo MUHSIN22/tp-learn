2010589526476964d70ab51f6645989043cfad5a
kartiks-diagright <kartik.sharma@diagright.com>
2022-07-08 11:23:51 +0530

    .

--- src/App.js
+++ src/App.js
@@ -20,11 +20,8 @@ import Sidebar from './Components/Sidebar/Sidebar';
 import Membership from './Components/Memberships/Memberships';
 import CVReview from './Components/CVEditor/CVReview'
 import DummyForm from './Components/EditForms/ProfilePicture'
-<<<<<<< HEAD
 import CognitiveSkills from './Components/Form/CognitiveSkills';
-=======
 import Resume from './Components/ResumeTemplate/Resume';
->>>>>>> 8fc106b2b4bebe27f16077757d41803a9dc93b0c
 function App() {
   const dispatch = useDispatch()
   const location = useLocation();
